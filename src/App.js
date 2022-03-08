e74c533b8c41e7e8a50445ce87fb64d1f543ba58
Mitch Fox <mitchfox@Mitchs-Mac-mini.local>
2022-03-08 09:04:13 +0000

    animation demo complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, Component, useRef, useContext, input } from 'react';
+import SwitchingMockups from './components/SwitchingMockups';
+
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='content'>
+        <SwitchingMockups />
+      </div>
     </div>
   );
 }
