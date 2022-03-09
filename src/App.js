ed48b9d7173c1442901d1ddf899abf2fe3b93379
Mitch Fox <mitchfox@Mitchs-Mac-mini.local>
2022-03-09 20:33:24 +0000

    finished homepage layout

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import React, { useState, useEffect, Component, useRef, useContext, input } from
 import SwitchingMockups from './components/SwitchingMockups';
 
 import './App.css';
+import { Row, Col } from 'react-simple-flex-grid';
+import "react-simple-flex-grid/lib/main.css";
+import { bounce, bounceInDown, bounceOutUp, headShake, jello } from 'react-animations';
+import 'animate.css';
+import Radium, { StyleRoot } from 'radium';
 
-
+import Test1 from './assets/mockupImages/test1.svg';
 
 function App() {
   return (
     <div className='app'>
       <div className='content'>
-        <SwitchingMockups />
+        <div className='container'>
+          <p>hi</p>
+            <Row className='rowContainer' justify="center" align="middle">
+                <Col span={4} className={'mockupColumn1'}>
+                    {/* <div style={styles.firstAppMockup} className='appImage1'></div> */}
+                    <div className='appImage1'>
+                        <img src={Test1} className='mockupImages'></img>
+                    </div>
+                </Col>
+                <Col span={4} className={'mockupColumn2'}>
+                    <div className='appImage2'>
+                        <img src={Test1} className='mockupImages'></img>
+                    </div>
+                </Col >
+                <Col span={4} className={'mockupColumn3'}>
+                    <div className='appImage3'>
+                        <img src={Test1} className='mockupImages'></img>
+                    </div>
+                </Col>
+            </Row>
+        </div>
       </div>
     </div>
   );
--- src/components/SwitchingMockups.jsx
+++ src/components/SwitchingMockups.jsx
@@ -2,31 +2,16 @@ import React, { useState, useEffect, Component, useRef, useContext, input } from
 import '../styles/SwitchingMockups.css';
 
 // Packages 
-import { Row, Col } from 'react-simple-flex-grid';
-import "react-simple-flex-grid/lib/main.css";
-import { bounce, bounceInDown, bounceOutUp, headShake, jello } from 'react-animations';
-import 'animate.css';
-import Radium, { StyleRoot } from 'radium';
+
+
+
 
 // 3 App Mockups with Interchanging Images. Subtle Animations. Infinite. Pause on Hover
 
 
 const SwitchingMockups = () => {
     return (
-
-        <Row className='rowContainer' align="middle" justify="center">
-            <Col span={4} className={'mockupColumn1'}>
-                {/* <div style={styles.firstAppMockup} className='appImage1'></div> */}
-                <div className='appImage1'></div>
-            </Col>
-            <Col span={4} className={'mockupColumn2'}>
-                <div className='appImage2'></div>
-            </Col >
-            <Col span={4} className={'mockupColumn3'}>
-                <div className='appImage3'></div>
-            </Col>
-        </Row>
-
+<div></div>
     )
 }
 
